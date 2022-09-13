9808ae52a8820fdfb2a23adbf38e34afc049fd90
Kominov <kamikadze66@narod.ru>
2022-09-13 13:02:03 +0500

    decomposition webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import path from "path";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = options;
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].bundle.js',
+            path: paths.build,
+            clean: true,
+        },
+        devtool: isDev ? 'inline-source-map' : undefined,
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,24 @@
-import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import path from "path";
 
-const config: webpack.Configuration = {
-    mode: "development",
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: '[name].[contenthash].bundle.js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin(
-            {template: path.resolve(__dirname, 'public', 'index.html')}
-        ),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-}
 
-export default config;
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
+        html: path.resolve(__dirname, 'public', 'index.html'),
+        build: path.resolve(__dirname, 'build'),
+        entry: path.resolve(__dirname, 'src', 'index.ts')
+    }
+    const mode = env.mode || 'development';
+    const PORT = env.port || 3000;
+
+    const isDev = mode === 'development'
+
+    return buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT
+    });
+};
