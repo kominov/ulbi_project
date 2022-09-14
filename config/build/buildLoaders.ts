2c368ea34b91a62f05ed92b59a2bfd926fc0c84f
Kominov <kamikadze66@narod.ru>
2022-09-14 14:14:39 +0500

    plugged scss moduls

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,26 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule [] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule [] {
+    const sassLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]__[hash:base64:6]' : '[hash:base64:8]'
+                    },
+
+                },
+            },
+            "sass-loader",
+        ],
+    };
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -9,5 +29,6 @@ export function buildLoaders(): webpack.RuleSetRule [] {
 
     return [
         typescriptLoader,
+        sassLoader
     ]
 }
